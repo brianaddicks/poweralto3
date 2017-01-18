88ec43d491303a1675cd3ce529ba9d794100d213
Brian Addicks <brian@addicks.us>
2017-01-17 20:22:26 -0500

    started getApiUrl method, added protocol handling

--- src/classes/PaDevice.ps1
+++ src/classes/PaDevice.ps1
@@ -3,4 +3,31 @@ class PaDevice {
     [int]$Port
     [string]$ApiKey
     [string]$Protocol
+
+    # Constructor
+    PaDevice () {
+    }
+
+    [String] getApiUrl() {
+        switch ($this.Protocol) {
+            "http"  { }
+            default { $this.Protocol = "https" }
+        }
+
+        $url = $this.Protocol + "://"
+        return $url
+    }
+
 }
+<#
+
+        public string ApiUrl {
+            get {
+                if ( !string.IsNullOrEmpty( this.Protocol ) && !string.IsNullOrEmpty( this.Device ) && this.Port > 0 ) {
+                    return this.Protocol + "://" + this.Device + ":" + this.Port + "/api/";
+                } else {
+                    return null;
+                }
+            }
+        }
+#>
