705ff3e35def6a2368d1203290bf3b85c43a4958
Brian Addicks <brian@addicks.us>
2017-01-26 10:06:09 -0500

    added invokeKeygenQuery to PaDevice

--- src/classes/PaDevice.ps1
+++ src/classes/PaDevice.ps1
@@ -1,23 +1,11 @@
-class HelperWebTools {
-    static [string] createQueryString ([hashtable]$hashTable) {
-        $i = 0
-        $queryString = "?"
-        foreach ($hash in $hashTable.GetEnumerator()) {
-            $i++
-            $queryString += $hash.Name + "=" + $hash.Value
-            if ($i -lt $HashTable.Count) {
-                $queryString += "&"
-            }
-        }
-        return $queryString
-    }
-}
-
 class PaDevice {
     [string]$Device
     [int]$Port = 443
     [string]$ApiKey
     [string]$Protocol = "https"
+
+    # Track usage
+    [array]$UrlHistory
 
     # Constructor
     PaDevice ([string]$Device) {
@@ -33,11 +21,39 @@ class PaDevice {
         }
     }
 
-    [Xml] invokeConfigQuery([string]$xPath,[string]$action) {
-        $url = $this.getApiUrl() + "type=config&xpath=$xPath&action=show"
-        $result = Invoke-WebRequest -Uri $url -SkipCertificateCheck
-        $result = [xml]($result.Content)
+    # Base API Query
+    [Xml] invokeApiQuery([hashtable]$queryString) {
+        $formattedQueryString = [HelperWebTools]::createQueryString($queryString)
+        $url = $this.getApiUrl() + $formattedQueryString
+        if ($queryString.type -ne "keygen") {
+            $this.UrlHistory += $url
+        } else {
+            $this.UrlHistory += $url -replace $queryString.password,"PASSWORDREDACTED"
+        }
+        $rawResult = Invoke-WebRequest -Uri $url -SkipCertificateCheck
+        $result = [xml]($rawResult.Content)
         return $result
     }
 
+    # Config API Query
+    [Xml] invokeConfigQuery([string]$xPath,[string]$action) {
+        $queryString = @{}
+        $queryString.key = $this.ApiKey
+        $queryString.type = "config"
+        $queryString.action = $action
+        $queryString.xpath = $xPath
+        $result = $this.invokeApiQuery($queryString)
+        return $result
+    }
+
+    # Keygen API Query
+    [xml] invokeKeygenQuery([string]$user,[string]$password) {
+        $queryString = @{}
+        $queryString.type = "keygen"
+        $queryString.user = $user
+        $queryString.password = $password
+        $result = $this.invokeApiQuery($queryString)
+        $this.ApiKey = $result.response.result.key
+        return $result
+    }
 }
