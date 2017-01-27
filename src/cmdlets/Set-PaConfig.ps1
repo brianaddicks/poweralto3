a867761a73e87ba626a442be8f9f153fa98d2d44
Brian Addicks <brian@addicks.us>
2017-01-27 17:07:45 -0500

    just some whitespace

--- src/cmdlets/Set-PaConfig.ps1
+++ src/cmdlets/Set-PaConfig.ps1
@@ -6,7 +6,7 @@ function Set-PaConfig {
         [Parameter(Mandatory=$True,Position=1,ParameterSetName="manual")]
 		[string]$ElementAsString,
 
-        [Parameter(Mandatory=$True,Position=1,ParameterSetName="object")]
+        [Parameter(Mandatory=$True,Position=1,ParameterSetName="object",ValueFromPipeline=$true)]
 		$PaObject
     )
     
@@ -15,8 +15,10 @@ function Set-PaConfig {
     if ($global:PaDeviceObject.Connected) {
         if ($PaObject) {
             Write-Verbose "$VerbosePrefix Getting info from Object"
+
             $ElementAsString = $PaObject.PrintPlainXml()
             $Xpath           = $PaObject.getXPath()
+            
             Write-Verbose "$VerbosePrefix Element: $ElementAsString"
             Write-Verbose "$VerbosePrefix Xpath: $Xpath"
         }
