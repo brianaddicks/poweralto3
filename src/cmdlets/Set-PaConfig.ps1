f5b5210b2855b8857287be4591986954192657c9
Brian Addicks <brian@addicks.us>
2017-01-27 16:45:39 -0500

    added Set-PaConfig

---
+++ src/cmdlets/Set-PaConfig.ps1
@@ -0,0 +1,27 @@
+function Set-PaConfig {
+	Param (
+		[Parameter(Mandatory=$True,Position=0,ParameterSetName="manual")]
+		[string]$Xpath = "/config",
+
+        [Parameter(Mandatory=$True,Position=1,ParameterSetName="manual")]
+		[string]$ElementAsString,
+
+        [Parameter(Mandatory=$True,Position=1,ParameterSetName="object")]
+		$PaObject
+    )
+    
+    $VerbosePrefix = "Set-PaConfig:"
+
+    if ($global:PaDeviceObject.Connected) {
+        if ($PaObject) {
+            Write-Verbose "$VerbosePrefix Getting info from Object"
+            $ElementAsString = $PaObject.PrintPlainXml()
+            $Xpath           = $PaObject.getXPath()
+            Write-Verbose "$VerbosePrefix Element: $ElementAsString"
+            Write-Verbose "$VerbosePrefix Xpath: $Xpath"
+        }
+        return $global:PaDeviceObject.invokeConfigQuery($Xpath,"set",$ElementAsString)
+    } else {
+        Throw "$VerbosePrefix Not Connected, please use Get-PaConfig to connect before using other cmdlets."
+    }
+}
