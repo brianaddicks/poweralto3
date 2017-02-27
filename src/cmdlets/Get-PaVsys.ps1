ce0e0e5d985ed85cf483f4a529941291ea728140
Brian Addicks <brian@addicks.us>
2017-02-27 15:02:57 -0500

    first stab at Get-PaVsys

---
+++ src/cmdlets/Get-PaVsys.ps1
@@ -0,0 +1,25 @@
+function Get-PaVsys {
+	Param (
+    )
+    
+    $VerbosePrefix = "Get-PaVsys:"
+
+    if ($global:PaDeviceObject.Connected) {
+        # Get the data
+        $Operation = '<show><system><state><filter-pretty>cfg.dns-vsys</filter-pretty></state></system></show>'
+        $Result = Invoke-PaOperation $Operation
+        
+        # Sanatize it and add it to the array
+        $Result = $Result.response.result.'#cdata-section' -replace "cfg.dns-vsys: ",""
+        $Rx = [regex] "(.+?):"
+        $Matches = $Rx.Matches($Result)
+        $ReturnObject = @()
+        foreach ($Match in $Matches) {
+            $ReturnObject += ($Match.Groups[1].Value).Trim()
+        }
+        
+        return $ReturnObject
+    } else {
+        Throw "$VerbosePrefix Not Connected, please use Get-PaConfig to connect before using other cmdlets."
+    }
+}
