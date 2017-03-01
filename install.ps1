2466b94387b0caadf71396a6b70d9f259ed2585a
Brian Addicks <brian@addicks.us>
2017-03-01 15:10:27 -0500

    functionized install.ps1

--- install.ps1
+++ install.ps1
@@ -1,42 +1,43 @@
-[CmdletBinding()]
-Param (
-    [Parameter(Mandatory=$False,Position=0)]
-    [string]$Destination
-)
+function DownloadPowerAlto3 {
+    [CmdletBinding()]
+    Param (
+        [Parameter(Mandatory=$False,Position=0)]
+        [string]$Destination
+    )
+    $ModuleName = 'PowerAlto3'
 
-$ModuleName = 'PowerAlto3'
-
-if ($Destination) {
-    $ResolvedPath = Resolve-Path $Destination
-    if (Test-Path $ResolvedPath) {
-        $DesiredPath = $ResolvedPath
-    } else {
-        Throw "Invalid Path Specified."
-    }
-} else {
-    if (Test-Path env:\PSMODULEPATH) {
-        $ModulePath = $env:PSMODULEPATH
-        if ($ModulePath -match ";") {
-            # Windows System
-            $SplitChar = ";"
+    if ($Destination) {
+        $ResolvedPath = Resolve-Path $Destination
+        if (Test-Path $ResolvedPath) {
+            $DesiredPath = $ResolvedPath
         } else {
-            $SplitChar = ":"
+            Throw "Invalid Path Specified."
         }
-        $Split = $ModulePath -split $SplitChar
-        $DesiredPath = $Split | ? { $_ -match "User" }
     } else {
-        $DesiredPath = Join-Path -Path (Get-Location) -ChildPath $ModuleName
+        if (Test-Path env:\PSMODULEPATH) {
+            $ModulePath = $env:PSMODULEPATH
+            if ($ModulePath -match ";") {
+                # Windows System
+                $SplitChar = ";"
+            } else {
+                $SplitChar = ":"
+            }
+            $Split = $ModulePath -split $SplitChar
+            $DesiredPath = $Split | ? { $_ -match "User" }
+        } else {
+            $DesiredPath = Join-Path -Path (Get-Location) -ChildPath $ModuleName
+        }
     }
-}
 
-# Create Directory
-$CreateDirectory = New-Item (Join-Path -Path $DesiredPath -ChildPath $ModuleName) -ItemType Container
-$OutputDirectory = Join-Path -Path $DesiredPath -ChildPath $ModuleName
+    # Create Directory
+    $CreateDirectory = New-Item (Join-Path -Path $DesiredPath -ChildPath $ModuleName) -ItemType Container
+    $OutputDirectory = Join-Path -Path $DesiredPath -ChildPath $ModuleName
 
-$Files = @("poweralto3.psd1","poweralto3.psm1")
+    $Files = @("poweralto3.psd1","poweralto3.psm1")
 
-foreach ($File in $Files) {
-    $Url = "https://raw.githubusercontent.com/brianaddicks/poweralto3/master/poweralto3/$File"    
-    $OutputPath = Join-Path -Path $OutputDirectory -ChildPath $File
-    Invoke-WebRequest -Uri $Url -OutFile $OutputPath
+    foreach ($File in $Files) {
+        $Url = "https://raw.githubusercontent.com/brianaddicks/poweralto3/master/poweralto3/$File"    
+        $OutputPath = Join-Path -Path $OutputDirectory -ChildPath $File
+        Invoke-WebRequest -Uri $Url -OutFile $OutputPath
+    }
 }
