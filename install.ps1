8e346897635953ccab9c92040df83864c1c9023d
Brian Addicks <brian@addicks.us>
2017-03-01 14:34:29 -0500

    added install script

---
+++ install.ps1
@@ -0,0 +1,26 @@
+$ModuleName = 'PowerAlto3'
+if (Test-Path env:\PSMODULEPATH) {
+    $ModulePath = $env:PSMODULEPATH
+    if ($ModulePath -match ";") {
+        # Windows System
+        $SplitChar = ";"
+    } else {
+        $SplitChar = ":"
+    }
+    $Split = $ModulePath -split $SplitChar
+    $DesiredPath = $Split | ? { $_ -match "User" }
+} else {
+    $DesiredPath = Join-Path -Path (Get-Location) -ChildPath $ModuleName
+}
+
+# Create Directory
+$CreateDirectory = New-Item (Join-Path -Path $DesiredPath -ChildPath $ModuleName) -ItemType Container
+$OutputDirectory = Join-Path -Path $DesiredPath -ChildPath $ModuleName
+
+$Files = @("poweralto3.psd1","poweralto3.psm1")
+
+foreach ($File in $Files) {
+    $Url = "https://raw.githubusercontent.com/brianaddicks/poweralto3/master/poweralto3/$File"    
+    $OutputPath = Join-Path -Path $OutputDirectory -ChildPath $File
+    Invoke-WebRequest -Uri $Url -OutFile $OutputPath
+}
